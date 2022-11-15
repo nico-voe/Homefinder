5d96be26630f2a025badc1a520b109e9ee4c5737
Nico <96868692+nico-voe@users.noreply.github.com>
2022-11-15 12:09:25 +0100

    feat: html and css for buttons for signup and login
    html and css only

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="box-1">
+        <div className="btn btn-one">
+          <span>SIGN UP AS A GUEST</span>
+        </div>
+      </div>
+
+      <div className="box-2">
+        <div className="btn btn-two">
+          <span>SIGN UP AS A HOMEOWNER</span>
+        </div>
+      </div>
+
+      <div className="box-3">
+        <div className="btn btn-three">
+          <span>LOGIN</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
