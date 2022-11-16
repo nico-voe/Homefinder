ba6830fe6bd06467bdac947877d80b741207f8ba
Nico <96868692+nico-voe@users.noreply.github.com>
2022-11-16 15:02:53 +0100

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -9,14 +9,20 @@ const Signup = () => {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Signup</h2>
+          <h2 className="text-center mb-4">Sign Up</h2>
           <Form>
-            <Form.Group id="email">Email</Form.Group>
-            <Form.Control type="email" ref={emailRef} required />
-            <Form.Group id="password">Password</Form.Group>
-            <Form.Control type="email" ref={passwordRef} required />
-            <Form.Group id="password-confirm">Password Confirmation</Form.Group>
-            <Form.Control type="password" ref={passwordConfirmRef} required />
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="email" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
             <Button className="w-100" type="submit">
               SIGN UP
             </Button>
