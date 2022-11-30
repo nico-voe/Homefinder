a731dbdbba1271a16c42b3108b68e72333022e3d
Nico <96868692+nico-voe@users.noreply.github.com>
2022-11-30 12:33:30 +0100

    fix: error fixed
    import useAuth missing

--- src/components/Regsiter.jsx
+++ src/components/Regsiter.jsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 const Register = () => {
-  const [email, setEmail] = useState([]);
-  const [password, setPassword] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(false);
+
+      //go to db
+      await register({ email, password });
+      setLoading(true);
+
+      //navigate to next page
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <label>Email</label>
         <input
           type="text"
